1eca2047a48af9b73f57ee4ceb895868d10e02ab
sp11ker <71824282+sp11ker@users.noreply.github.com>
2025-08-26 14:45:35 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,3 +1,108 @@
-# Azure onboarding script
+provider "azurerm" {
+  features {}
+}
 
+# 1. Resource Group
+resource "azurerm_resource_group" "main" {
+  name     = "rg-terraform-demo"
+  location = var.azure_region
+}
+
+# 2. Virtual Network
+resource "azurerm_virtual_network" "main" {
+  name                = "vnet-main"
+  address_space       = ["10.0.0.0/16"]
+  location            = azurerm_resource_group.main.location
+  resource_group_name = azurerm_resource_group.main.name
+}
+
+# 3. Subnet
+resource "azurerm_subnet" "main" {
+  name                 = "subnet-main"
+  resource_group_name  = azurerm_resource_group.main.name
+  virtual_network_name = azurerm_virtual_network.main.name
+  address_prefixes     = ["10.0.1.0/24"]
+}
+
+# 4. Public IP
+resource "azurerm_public_ip" "main" {
+  name                = "public-ip"
+  location            = azurerm_resource_group.main.location
+  resource_group_name = azurerm_resource_group.main.name
+  allocation_method   = "Dynamic"
+}
+
+# 5. Network Security Group (Allow SSH)
+resource "azurerm_network_security_group" "ssh" {
+  name                = "nsg-ssh"
+  location            = azurerm_resource_group.main.location
+  resource_group_name = azurerm_resource_group.main.name
+
+  security_rule {
+    name                       = "SSH"
+    priority                   = 1001
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "22"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+}
+
+# 6. NIC
+resource "azurerm_network_interface" "main" {
+  name                = "nic-main"
+  location            = azurerm_resource_group.main.location
+  resource_group_name = azurerm_resource_group.main.name
+
+  ip_configuration {
+    name                          = "ipconfig"
+    subnet_id                     = azurerm_subnet.main.id
+    private_ip_address_allocation = "Dynamic"
+    public_ip_address_id          = azurerm_public_ip.main.id
+  }
+}
+
+# 7. Associate NSG with NIC
+resource "azurerm_network_interface_security_group_association" "main" {
+  network_interface_id      = azurerm_network_interface.main.id
+  network_security_group_id = azurerm_network_security_group.ssh.id
+}
+
+# 8. SSH Key
+resource "tls_private_key" "example" {
+  algorithm = "RSA"
+  rsa_bits  = 4096
+}
+
+# 9. Linux VM
+resource "azurerm_linux_virtual_machine" "web" {
+  name                = "vm-web"
+  resource_group_name = azurerm_resource_group.main.name
+  location            = azurerm_resource_group.main.location
+  size                = "Standard_B1s"
+  admin_username      = "azureuser"
+  network_interface_ids = [
+    azurerm_network_interface.main.id
+  ]
+
+  admin_ssh_key {
+    username   = "azureuser"
+    public_key = tls_private_key.example.public_key_openssh
+  }
+
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
+
+  source_image_reference {
+    publisher = "Canonical"
+    offer     = "UbuntuServer"
+    sku       = "20_04-lts"
+    version   = "latest"
+  }
+}
 
